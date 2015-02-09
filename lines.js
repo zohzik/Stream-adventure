7d60998a236296456c047c5d8b59a5769aa3480a
zohzik3 <taymyr1026@gmail.com>
2015-02-09 14:43:18 +0000

    working now, problem was i had the function inside of through() named as something and that somehow messed with how the code worked. remmoving the name fixed the problem.

--- lines.js
+++ lines.js
@@ -1,23 +1,14 @@
 var through = require('through');
 var split = require("split");
-var counter =10;
-var value;
+var even = false;
 
-function alternate(){
-    
-    if(counter===11){
-        return function(buffer){
-            value = buffer.toString().toLowerCase() + "\n";
-            counter=10;
-            this.queue(value);
-        };
-    }else if(counter===10){
-        return function(buffer){
-            value = buffer.toString().toUpperCase() + "\n";
-            counter=11;
-            this.queue(value);
-        };
+process.stdin.pipe(split()).pipe(through(function(buffer){
+    if(!even){
+        this.queue(buffer.toString().toLowerCase());
+        even = true;
+    }else{
+        this.queue(buffer.toString().toUpperCase());
+        even=false;
     }
-}
-
-process.stdin.pipe(split()).pipe(through(alternate(), function end(){this.queue(null);})).pipe(process.stdout);
+    this.queue("\n");
+})).pipe(process.stdout);
